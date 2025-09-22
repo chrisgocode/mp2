853ae8f6d84f624c14e8dafe8855fd8e012804b0
Christian Gonzalez <chrisgo@bu.edu>
2025-09-22 00:39:39 -0400

    add background color to cards specific to each type

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,22 @@
 import styled from "styled-components";
 import type { Pokemon } from "pokenode-ts";
+import { POKEMON_TYPE_COLORS } from "../utils/pokemonTypes";
 
-const CardContainer = styled.div`
+// passing in a background color based on the type of the pokemon
+const CardContainer = styled.div<{ backgroundColor: string }>`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100%;
   height: 100%;
+  background-color: ${(props) => props.backgroundColor};
+  border: none;
+  border-radius: 25px;
+  transition: all 0.3s ease;
+  &:hover {
+    transform: scale(1.05);
+  }
 `;
 
 const PokemonCardImage = styled.img`
@@ -23,8 +32,14 @@ const PokemonCardName = styled.h1`
 `;
 
 export default function PokemonCard(props: { pokemon: Pokemon }) {
+  // gets the primary type of the pokemon to determine its main color
+  const primaryType = props.pokemon.types[0]?.type
+    .name as keyof typeof POKEMON_TYPE_COLORS;
+  // sets the background color based on the primary type
+  const backgroundColor = POKEMON_TYPE_COLORS[primaryType] || "#FFFFFF";
+
   return (
-    <CardContainer>
+    <CardContainer backgroundColor={backgroundColor}>
       <PokemonCardImage
         src={props.pokemon.sprites.front_default ?? ""}
         alt={`Image of ${props.pokemon.name}`}
